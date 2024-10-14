1fd46f95433ce585718364796c59d7f5c3d2078f
uvic-dot6 <uday.victoria@gmail.com>
2024-10-14 01:16:38 -0600

    Map displament
    El mapa se acomodo para que ya no se salga de los bordes

--- classes/AGENT_class.py
+++ classes/AGENT_class.py
@@ -1,24 +1,23 @@
 
 class Agent_Human:
-pos_i = None
-pos_f = None
-costo_actual = None
-costo_final = None
-cost_movement = {
-    "Mountain": None
-    "Earth": 1
-    "Water": 2
-    "Sand": 3
-    "Forest": 4
-    "Swamp": 5
-    "Snow": 5
-}
-    def __init__(self):
+    pos_i = None
+    pos_f = None
+    costo_actual = None
+    costo_final = None
+    cost_movement= {
+        "Mountain": None
+        "Earth": 1
+        "Water": 2
+        "Sand": 3
+        "Forest": 4
+        "Swamp": 5
+        "Snow": 5
+    }
+        def __init__(self):
 
-    def one_sensor(self, pos_x, pos_y, offset_x, offset_y):
-        #unmask la casilla en la que apunta
-    def four_sensors(self, pos_x, pos_y, offset_x, offset_y):
-        #unmask las cuatro casillas alrededor
-    def calculate_cost(self, pos_x, pos_y):
-        self.costo_actual += cost_movement.get()
-
+        def one_sensor(self, pos_x, pos_y, offset_x, offset_y):
+            #unmask la casilla en la que apunta
+        def four_sensors(self, pos_x, pos_y, offset_x, offset_y):
+            #unmask las cuatro casillas alrededor
+        def calculate_cost(self, pos_x, pos_y):
+            self.costo_actual += cost_movement.get()
